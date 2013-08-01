6252567eec9c263bcc48f1c065d83a4d9d9e3852
Luis Cabellos <zhen.sydow@gmail.com>
2013-08-01 11:21:32 +0200

    add colorized objects

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,9 @@ int main(){
     std::cout << "antialiasing level: " << settings.antialiasingLevel << std::endl;
     std::cout << "version: " << settings.majorVersion << "." << settings.minorVersion << std::endl;
 
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
     sf::Font font;
     if( !font.loadFromFile( "GentiumPlus-R.ttf") ){
         std::cout << "Error loading Font\n";
@@ -67,15 +70,32 @@ int main(){
        0.0f,  1.0f, -3.0f,
     };
 
-    // This will identify our vertex buffer
-    GLuint vertexbuffer;
+    static const GLfloat g_color_buffer_data[] = {
+        0.583f,  0.771f,  0.014f,
+        0.609f,  0.115f,  0.436f,
+        0.327f,  0.483f,  0.844f,
+        0.822f,  0.569f,  0.201f,
+        0.435f,  0.602f,  0.223f,
+        0.310f,  0.747f,  0.185f,
+        0.597f,  0.770f,  0.761f,
+        0.559f,  0.436f,  0.730f,
+        0.359f,  0.583f,  0.152f,
+    };
 
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
-    glGenBuffers(1, &vertexbuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Give our vertices to OpenGL.
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    // This will identify our vertex buffer
+    GLuint vertexbuffer, colorbuffer;
+
+    // Generate buffers
+    glGenBuffers( 1, &vertexbuffer );
+    glGenBuffers( 1, &colorbuffer );
+
+    //put buffer data on OpenGL
+    glBindBuffer( GL_ARRAY_BUFFER, vertexbuffer );
+    glBufferData( GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
+                  g_vertex_buffer_data, GL_STATIC_DRAW );
+    glBindBuffer( GL_ARRAY_BUFFER, colorbuffer );
+    glBufferData( GL_ARRAY_BUFFER, sizeof(g_color_buffer_data),
+                  g_color_buffer_data, GL_STATIC_DRAW );
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     GLuint programID = loadProgram( "test01" );
@@ -141,7 +161,7 @@ int main(){
         glUseProgram( programID );
 
         // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray( 0 );
         glBindBuffer( GL_ARRAY_BUFFER, vertexbuffer );
         glVertexAttribPointer(
            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
@@ -151,6 +171,11 @@ int main(){
            0,                  // stride
            (void*)0            // array buffer offset
         );
+
+        glEnableVertexAttribArray( 1 );
+        glBindBuffer( GL_ARRAY_BUFFER, colorbuffer );
+        glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE,
+                               0, (void*)0 );
 
         // Draw the triangles
         model = glm::mat4(1.0f);
