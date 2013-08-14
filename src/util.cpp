27adfdfb4d0fd85d5ae58bfe1e318d96a9a6f12f
Luis Cabellos <zhen.sydow@gmail.com>
2013-08-14 11:22:16 +0200

    real prandom height generation

--- src/util.cpp
+++ src/util.cpp
@@ -5,11 +5,19 @@
 */
 //------------------------------------------------------------------------------
 #include "util.hpp"
+#include <random>
 
 //------------------------------------------------------------------------------
 Chunk createRandomChunk( float min, float max ){
     Chunk chunk;
 
+    std::random_device rd;
+    std::mt19937 gen( rd() );
+    std::uniform_real_distribution<> dis( min, max );
+
+    for( int i = 0 ; i < 7 ; ++i ){
+        chunk.setHeight( 0, dis( gen ) );
+    }
 
     return chunk;
 }
