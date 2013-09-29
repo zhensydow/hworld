b09b89e600ed8b564f17a8bf1c5dbf050916af80
Luis Cabellos <zhen.sydow@gmail.com>
2013-09-29 17:31:13 +0200

    remove gamestate factory

--- src/gamestatefactory.cpp
+++
@@ -1,7 +0,0 @@
-/** @file gamestatefactory.cpp
-    @brief GameStateFactory definitions.
-    @author Luis Cabellos
-    @date 2013-09-29
-*/
-//------------------------------------------------------------------------------
-#include "gamestatefactory.hpp"
