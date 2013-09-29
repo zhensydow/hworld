79a4023dc902e1f27093e976cd1b842f8f80fe2c
Luis Cabellos <zhen.sydow@gmail.com>
2013-09-29 19:01:01 +0200

    multiple state example

--- data/state/test.lua
+++ data/state/test.lua
@@ -1,9 +1,16 @@
+count = 0
+
 function start()
    print( 'Start state' )
 end
 
 function update( dt )
+   count = count + 1
    print( 'Update state ' .. dt .. ' and ' .. count )
+   if count > 10 then
+      engine.newstate( 'test2' )
+      count = 0
+   end
 end
 
 function stop()
---
+++ data/state/test2.lua
@@ -0,0 +1,11 @@
+function start()
+   print( 'Start state test2' )
+end
+
+function update( dt )
+   print( 'Update state ' .. dt .. ' test2' )
+end
+
+function stop()
+   print( 'Stop state test2' )
+end
