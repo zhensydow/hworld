75cbd67744054ab8167b60cf031462bd5d4225a0
Luis Cabellos <zhen.sydow@gmail.com>
2013-10-20 18:00:37 +0200

    add camera fov in component

--- src/c_camera.hpp
+++ src/c_camera.hpp
@@ -4,6 +4,8 @@
 
 //------------------------------------------------------------------------------
 #include "component.hpp"
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
 
 //------------------------------------------------------------------------------
 class CCamera : public Component {
@@ -14,13 +16,36 @@ public:
 
     ComponentType getType() const override;
 
+    glm::mat4 getProjection() const;
+    float getFov() const;
+    void setFov( float f );
+
 private:
+    float m_fov {45.0f};
 };
 
 //------------------------------------------------------------------------------
 inline
 ComponentType CCamera::getType() const{
     return type;
+}
+
+//------------------------------------------------------------------------------
+inline
+glm::mat4 CCamera::getProjection() const{
+    return glm::perspective( m_fov, 4.0f / 3.0f, 0.1f, 100.0f );
+}
+
+//------------------------------------------------------------------------------
+inline
+float CCamera::getFov() const{
+    return m_fov;
+}
+
+//------------------------------------------------------------------------------
+inline
+void CCamera::setFov( float f ){
+    m_fov = f;
 }
 
 //------------------------------------------------------------------------------
