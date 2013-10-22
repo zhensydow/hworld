9012c8b3829595a3bda1ce62546f27a08f37e1a8
Luis Cabellos <zhen.sydow@gmail.com>
2013-10-22 08:43:30 +0200

    load lua in script component

--- src/c_script.cpp
+++ src/c_script.cpp
@@ -5,6 +5,12 @@
 */
 //------------------------------------------------------------------------------
 #include "c_script.hpp"
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include "util.hpp"
+
+//------------------------------------------------------------------------------
+using namespace boost::filesystem;
 
 //------------------------------------------------------------------------------
 CScript::CScript( Entity & e ) : Component( e ) {
@@ -13,6 +19,25 @@ CScript::CScript( Entity & e ) : Component( e ) {
 
 //------------------------------------------------------------------------------
 void CScript::load( const std::string & filename ){
+    if( !is_regular_file(filename) ){
+        std::cout << "Not file '" << filename << "'" << std::endl;
+    }
+
+    // Lua Initialization
+    m_lua = luaL_newstate();
+    if( !m_lua ){
+        std::cout << "Can't create Lua State" << std::endl;
+    }
+
+    lua_gc( m_lua, LUA_GCSTOP, 0 );
+    luaL_openlibs( m_lua );
+    lua_gc( m_lua, LUA_GCRESTART, 0 );
+
+    // execute class file
+    auto ret = luaL_dofile( m_lua, filename.c_str() );
+    if( !checkLuaReturn( m_lua, ret ) ){
+        m_lua = nullptr;
+    }
 }
 
 //------------------------------------------------------------------------------
