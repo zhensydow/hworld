146ee876d30f5e9c42e7db1ac394caa4f0a22155
Luis Cabellos <zhen.sydow@gmail.com>
2013-10-22 08:10:06 +0200

    add tranform position s&g·

--- src/c_transform.hpp
+++ src/c_transform.hpp
@@ -20,8 +20,11 @@ public:
 
     ComponentType getType() const override;
 
+    void setPosition( const glm::vec3 & pos );
+    glm::vec3 getGlobalPosition() const;
+
 private:
-    glm::vec3 m_pos;
+    glm::vec3 m_pos = glm::vec3( 0.0, 0.0, 0.0 );
     glm::vec3 m_rot;
 };
 
@@ -30,6 +33,18 @@ inline
 ComponentType CTransform::getType() const{
     return type;
 }
+
+//------------------------------------------------------------------------------
+inline
+glm::vec3 CTransform::getGlobalPosition() const{
+    return m_pos;
+};
+
+//------------------------------------------------------------------------------
+inline
+void CTransform::setPosition( const glm::vec3 & pos ){
+    m_pos = pos;
+};
 
 //------------------------------------------------------------------------------
 #endif//C_TRANSFORM_HPP_
