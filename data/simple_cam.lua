a2543beb80d1563017afe9274a49cf0fa1ff9e9e
Luis Cabellos <zhen.sydow@gmail.com>
2013-10-24 18:50:05 +0200

    cam entity script test

--- data/simple_cam.lua
+++ data/simple_cam.lua
@@ -1,1 +1,16 @@
 print("hello")
+
+val = 0
+
+function onUpdate( delta )
+   --print( "Cam update " .. delta .. " " .. val )
+   val = val + delta
+   if input.isKeyPressed( input.U ) then
+      fov = entity.camera.getFov()
+      print( "Key U Pressed" )
+   end
+
+   if input.isKeyPressed( input.I ) then
+      print( "Key I Pressed" )
+   end
+end
