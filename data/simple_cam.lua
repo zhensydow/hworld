3a7846458a0800c896e0858b015c1ae6f67d7304
Luis Cabellos <zhen.sydow@gmail.com>
2013-10-25 17:28:08 +0200

    use method example in lua

--- data/simple_cam.lua
+++ data/simple_cam.lua
@@ -6,8 +6,8 @@ function onUpdate( delta )
    --print( "Cam update " .. delta .. " " .. val )
    val = val + delta
    if input.isKeyPressed( input.U ) then
-      fov = entity.camera.getFov()
-      print( "Key U Pressed" )
+      fov = entity.camera:getFov()
+      print( "Key U Pressed " .. fov )
    end
 
    if input.isKeyPressed( input.I ) then
