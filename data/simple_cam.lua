2e3c301421aed73d940749fc502d3113b6407134
Luis Cabellos <zhen.sydow@gmail.com>
2013-10-25 18:57:21 +0200

    change camera FOV from script

--- data/simple_cam.lua
+++ data/simple_cam.lua
@@ -6,11 +6,18 @@ function onUpdate( delta )
    --print( "Cam update " .. delta .. " " .. val )
    val = val + delta
    if input.isKeyPressed( input.U ) then
-      fov = entity.camera:getFov()
-      print( "Key U Pressed " .. fov )
+      fov = entity.camera:getFov() + 20*delta
+      if fov > 80.0 then
+         fov = 80.0
+      end
+      entity.camera:setFov( fov );
    end
 
    if input.isKeyPressed( input.I ) then
-      print( "Key I Pressed" )
+      fov = entity.camera:getFov() - 20*delta
+      if fov < 10.0 then
+         fov = 10.0
+      end
+      entity.camera:setFov( fov );
    end
 end
