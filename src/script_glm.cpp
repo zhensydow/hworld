99188b175e4836b6ff561f535d71c20e12a43929
Luis Cabellos <zhen.sydow@gmail.com>
2013-11-05 23:20:41 +0100

    better push of mat4x4 and vec4

--- src/script_glm.cpp
+++ src/script_glm.cpp
@@ -2,6 +2,23 @@
 #include "script.hpp"
 #include "glm/glm.hpp"
 #include "glm/ext.hpp"
+#include "util.hpp"
+
+//------------------------------------------------------------------------------
+void lua_push_vec4( lua_State * lua, const glm::vec4 & vec ){
+    auto lua_val = (glm::vec4 *)lua_newuserdata( lua, sizeof(glm::vec4) ); // +1
+    *lua_val = vec;
+    luaL_getmetatable( lua, "glm_vec4" );  // +2
+    lua_setmetatable( lua, -2 );           // +1
+}
+
+//------------------------------------------------------------------------------
+void lua_push_mat4x4( lua_State * lua, const glm::mat4x4 & mat ){
+    auto lua_val = (glm::mat4x4 *)lua_newuserdata( lua, sizeof(glm::mat4x4)); // +1
+    *lua_val = mat;
+    luaL_getmetatable( lua, "glm_mat4x4" ); // +2
+    lua_setmetatable( lua, -2 );            // +1
+}
 
 //------------------------------------------------------------------------------
 int glm_rotate( lua_State * lua ){
@@ -10,18 +27,13 @@ int glm_rotate( lua_State * lua ){
         return luaL_error( lua, "Got %d arguments expected 4", n );
     }
 
-    auto angle = luaL_checknumber( lua, 1 );
-    auto xa = luaL_checknumber( lua, 2 );
-    auto ya = luaL_checknumber( lua, 3 );
-    auto za = luaL_checknumber( lua, 4 );
+    auto angle = float( luaL_checknumber( lua, 1 ) );
+    auto xa = float( luaL_checknumber( lua, 2 ) );
+    auto ya = float( luaL_checknumber( lua, 3 ) );
+    auto za = float( luaL_checknumber( lua, 4 ) );
     auto rot = glm::rotate( angle, xa, ya, za );
 
-    glm::mat4x4 *mat = (glm::mat4x4 *)lua_newuserdata( lua, sizeof(glm::mat4x4));
-
-    *mat = rot;
-
-    luaL_getmetatable( lua, "glm_mat4x4");
-    lua_setmetatable( lua, -2);
+    lua_push_mat4x4( lua, rot );
 
     return 1;
 }
@@ -38,12 +50,7 @@ int glm_vec4( lua_State * lua ){
     auto z = luaL_checknumber( lua, 3 );
     auto w = luaL_checknumber( lua, 4 );
 
-    glm::vec4 * ret = (glm::vec4 *)lua_newuserdata( lua, sizeof(glm::vec4));
-
-    *ret = glm::vec4( x, y, z, w );
-
-    luaL_getmetatable( lua, "glm_vec4");
-    lua_setmetatable( lua, -2);
+    lua_push_vec4( lua, glm::vec4( x, y, z, w ) );
 
     return 1;
 }
