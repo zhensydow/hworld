76539a0e59d108e5d445fbba414b7f8ba607c043
Luis Cabellos <zhen.sydow@gmail.com>
2013-12-31 15:17:26 +0100

    add static mesh class

---
+++ src/staticmesh.cpp
@@ -0,0 +1,15 @@
+/** @file staticmesh.cpp
+    @brief StaticMesh definitions.
+    @author Luis Cabellos
+    @date 2013-12-31
+*/
+//------------------------------------------------------------------------------
+#include "staticmesh.hpp"
+#include "renderer.hpp"
+
+//------------------------------------------------------------------------------
+void StaticMesh::draw( Renderer & renderer ){
+    //renderer.render( *this );
+}
+
+//------------------------------------------------------------------------------
