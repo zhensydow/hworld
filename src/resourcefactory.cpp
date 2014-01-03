474c7d5940b37f2f5441c5f12f6869a01386fefe
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-03 12:39:09 +0100

    add resource factory skel

---
+++ src/resourcefactory.cpp
@@ -0,0 +1,14 @@
+/** @file resourcefactory.cpp
+    @brief ResourceFactory definitions.
+    @author Luis Cabellos
+    @date 2014-01-03
+*/
+//------------------------------------------------------------------------------
+#include "resourcefactory.hpp"
+
+//------------------------------------------------------------------------------
+std::shared_ptr<StaticModel> ResourceFactory::getSimpleModel( const std::string & name ){
+    return nullptr;
+}
+
+//------------------------------------------------------------------------------
---
+++ src/resourcefactory.hpp
@@ -0,0 +1,28 @@
+/** @file resourcefactory.hpp
+    @brief ResourceFactory declarations.
+    @author Luis Cabellos
+    @date 2014-01-03
+*/
+//------------------------------------------------------------------------------
+#ifndef RESOURCEFACTORY_HPP_
+#define RESOURCEFACTORY_HPP_
+
+//------------------------------------------------------------------------------
+#include <string>
+#include <memory>
+
+//------------------------------------------------------------------------------
+class StaticModel;
+
+//------------------------------------------------------------------------------
+class ResourceFactory {
+public:
+    std::shared_ptr<StaticModel> getSimpleModel( const std::string & name );
+
+private:
+};
+
+//------------------------------------------------------------------------------
+#endif//RESOURCEFACTORY_HPP_
+
+//------------------------------------------------------------------------------
