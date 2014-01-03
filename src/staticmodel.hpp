4152dd277ba2cd98567dcc0b484fccd0730bf03f
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-03 13:17:52 +0100

    add meshes in model

--- src/staticmodel.hpp
+++ src/staticmodel.hpp
@@ -10,8 +10,19 @@
 //------------------------------------------------------------------------------
 class StaticModel {
 public:
+    void insertMesh( std::unique_ptr<StaticMesh> mesh );
+
 private:
+    std::vector< std::unique_ptr<StaticMesh> > m_meshes;
 };
+
+//------------------------------------------------------------------------------
+inline
+void StaticModel::insertMesh( std::unique_ptr<StaticMesh> mesh ){
+    if( mesh ){
+        m_meshes.push_back( std::move(mesh) );
+    }
+}
 
 //------------------------------------------------------------------------------
 #endif//STATICMODEL_HPP_
