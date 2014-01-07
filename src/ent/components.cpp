bd3cb7fe00415696f36110193bcb12bfcc0166fb
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-07 12:52:10 +0100

    fix forgotten component

--- src/ent/components.cpp
+++ src/ent/components.cpp
@@ -6,6 +6,7 @@
 static std::unordered_map< std::string, ComponentType > s_ctNames = {
     { "null", ComponentType::CT_NULL },
     { "transform", ComponentType::CT_TRANSFORM },
+    { "staticmodel", ComponentType::CT_STATICMODEL },
     { "camera", ComponentType::CT_CAMERA },
     { "script", ComponentType::CT_SCRIPT },
 };
