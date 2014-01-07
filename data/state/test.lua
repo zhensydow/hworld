daaf2f9f01a1bc04b28ca21907f3d65dea6dea95
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-07 20:27:00 +0100

    add tree + rock in test example

--- data/state/test.lua
+++ data/state/test.lua
@@ -2,11 +2,18 @@ dtime = 0
 
 function start()
    terminal.newLine( 'Start state' )
+
+   tree = engine.newEntity( 'tree01' )
+   tree.transform:setPosition( 0, 1, 2 )
+
+   rock = engine.newEntity( 'rock01' )
+   rock.transform:setPosition( 0, 2, 0 )
 end
 
 function update( dt )
    dtime = dtime + dt
    --print( 'Update state ' .. dt .. ' and ' .. dtime )
+   --tree.transform:setPosition( 0, dtime / 4.0, 0 )
    if dtime > 1 then
       engine.newState( 'test2' )
       dtime = 0
