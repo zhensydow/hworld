d26636ec14b0fb885bcb13bea51a17d715e2ec4b
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-16 20:35:04 +0100

    load json config

--- src/config.cpp
+++ src/config.cpp
@@ -5,10 +5,45 @@
 */
 //------------------------------------------------------------------------------
 #include "config.hpp"
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include "json/json.h"
 
 //------------------------------------------------------------------------------
-std::unique_ptr<Config> loadConfig( const std::string & filename ){
-    return nullptr;
+using namespace std;
+using namespace boost::filesystem;
+
+//------------------------------------------------------------------------------
+unique_ptr<Config> loadConfig( const string & filename ){
+    ifstream ifs( filename, ifstream::binary );
+    if( !ifs ){
+        cout << "ERROR: Could not open " << filename << endl;
+        return nullptr;
+    }
+
+    ifs.seekg( 0, ifs.end );
+    auto length = ifs.tellg();
+    ifs.seekg( 0, ifs.beg );
+
+    string entityJson;
+    entityJson.resize(length, ' ');
+    auto begin = &*entityJson.begin();
+
+    ifs.read( begin, length );
+    ifs.close();
+
+    Json::Value root;
+    Json::Reader reader;
+
+    auto parsingRet = reader.parse( entityJson, root );
+    if( ! parsingRet ){
+        cout << "ERROR: invalid entity JSON parsing" << endl;
+        cout << reader.getFormattedErrorMessages();
+        return nullptr;
+    }
+
+    auto config = unique_ptr<Config>( new Config );
+    return config;
 }
 
 //------------------------------------------------------------------------------
