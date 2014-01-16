f4960c170260771757bce05af64eceb194d73c46
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-16 17:27:34 +0100

    add config load skel

---
+++ src/config.cpp
@@ -0,0 +1,14 @@
+/** @file config.cpp
+    @brief Configuration definitions.
+    @author Luis Cabellos
+    @date 2014-01-16
+*/
+//------------------------------------------------------------------------------
+#include "config.hpp"
+
+//------------------------------------------------------------------------------
+std::unique_ptr<Config> loadConfig( const std::string & filename ){
+    return nullptr;
+}
+
+//------------------------------------------------------------------------------
