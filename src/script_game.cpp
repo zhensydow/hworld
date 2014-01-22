09a24393a409a030aeddd4bfc22839f0d2bebed4
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-22 11:56:42 +0100

    set sundir in scripting

--- src/script_game.cpp
+++ src/script_game.cpp
@@ -1,7 +1,33 @@
+/** @file script_game.cpp
+    @brief Game script library.
+    @author Luis Cabellos
+    @date 2014-01-22
+*/
+//------------------------------------------------------------------------------
+#include "script.hpp"
+#include "engine.hpp"
+
+//------------------------------------------------------------------------------
+using namespace std;
+
+//------------------------------------------------------------------------------
+static int game_setSunDir( lua_State *ls ){
+    auto x = luaL_checknumber( ls, 1 );
+    auto y = luaL_checknumber( ls, 2 );
+    auto z = luaL_checknumber( ls, 3 );
+    auto & engine = Engine::instance();
+    auto & renderer = engine.getRenderer();
+
+    renderer.setSunDir( glm::vec3( x, y, z ) );
+
+    return 0;
+}
+
 //------------------------------------------------------------------------------
 /** List of functions of game lua library.
 */
 static const luaL_Reg gamelib[] = {
+    {"setSunDir", game_setSunDir},
     {nullptr, nullptr}
 };
 
