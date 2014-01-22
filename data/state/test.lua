d21b32bc8f533722581a0450c05ddee8ca243f9b
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-22 12:28:20 +0100

    add sun animation in test state

--- data/state/test.lua
+++ data/state/test.lua
@@ -11,13 +11,22 @@ function start()
 
    rock = engine.newEntity( 'rock01' )
    rock.transform:setPosition( 0, 2, 0 )
+
+   game.setSunDir( 0, -1, -1 );
 end
 
 function update( dt )
    dtime = dtime + dt
    --print( 'Update state ' .. dt .. ' and ' .. dtime )
    --tree.transform:setPosition( 0, dtime / 4.0, 0 )
-   if dtime > 1 then
+
+   angle1 = dtime*100
+   rot1 = glm.rotate( angle1, 0.0, 1.0, 0.0 )
+   axis2 = rot1:mul( glm.vec4( 0, -1, -1, 0.0 ) )
+
+   game.setSunDir( axis2:get(0), axis2:get(1), axis2:get(2) )
+
+   if dtime > 2 then
       engine.newState( 'test2' )
       dtime = 0
    end
