6cc0ce1e379e73593a6f556a476016f783487359
Luis Cabellos <zhen.sydow@gmail.com>
2014-01-23 14:46:34 +0100

    add gpl header

--- src/terminal.hpp
+++ src/terminal.hpp
@@ -1,3 +1,21 @@
+/**
+    Copyright 2014, HexWorld Authors.
+
+    This file is part of HexWorld.
+
+    HexWorld is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HexWorld is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HexWorld.  If not, see <http://www.gnu.org/licenses/>.
+**/
 /** @file terminal.hpp
     @brief Info
     @author Luis Cabellos
