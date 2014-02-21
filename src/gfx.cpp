72276900cdb8a0efdda50b9e8ae057f123c7c7a9
Luis Cabellos <zhen.sydow@gmail.com>
2014-02-21 22:20:04 +0100

    add sky color

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -270,6 +270,7 @@ void Gfx::startColorPass(){
     glDepthFunc( GL_LESS );
     glEnable( GL_CULL_FACE );
 
+    glClearColor( 0.0, 0.746, 1.0, 0.0 );
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 }
 
