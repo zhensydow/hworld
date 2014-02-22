48a4e0d5cef3875b5eb6b67d894f1b11ba8873c6
Luis Cabellos <zhen.sydow@gmail.com>
2014-02-22 17:27:34 +0100

    another world

--- src/world.cpp
+++ src/world.cpp
@@ -28,19 +28,43 @@
 //------------------------------------------------------------------------------
 World::World(){
     // create a minimal world
-    m_terrain[ 0 ] = createRandomChunk( -5, 5 );
-    m_terrain[ 1 ] = createRandomChunk( -3, 3 );
-    m_terrain[ 2 ] = createRandomChunk( -3, 3 );
-    m_terrain[ 3 ] = createRandomChunk( -3, 3 );
-    m_terrain[ 4 ] = createRandomChunk( -3, 3 );
-    m_terrain[ 5 ] = createRandomChunk( -3, 3 );
-    m_terrain[ 6 ] = createRandomChunk( -3, 3 );
+    m_terrain[ 0 ] = createRandomChunk( 0, 50 );
+    m_terrain[ 1 ] = createRandomChunk( 0, 2 );
+    m_terrain[ 2 ] = createRandomChunk( 0, 3 );
+    m_terrain[ 3 ] = createRandomChunk( 0, 3 );
+    m_terrain[ 4 ] = createRandomChunk( 0, 3 );
+    m_terrain[ 5 ] = createRandomChunk( 0, 3 );
+    m_terrain[ 6 ] = createRandomChunk( 0, 3 );
+    m_terrain[ 7 ] = createRandomChunk( -3, 3 );
+    m_terrain[ 8 ] = createRandomChunk( -3, 3 );
+    m_terrain[ 9 ] = createRandomChunk( -3, 3 );
+    m_terrain[ 10 ] = createRandomChunk( -3, 3 );
+    m_terrain[ 11 ] = createRandomChunk( -3, 3 );
+    m_terrain[ 12 ] = createRandomChunk( -3, 3 );
+    m_terrain[ 13 ] = createRandomChunk( -3, 1 );
+    m_terrain[ 14 ] = createRandomChunk( -3, 1 );
+    m_terrain[ 15 ] = createRandomChunk( -3, 1 );
+    m_terrain[ 16 ] = createRandomChunk( -3, 1 );
+    m_terrain[ 17 ] = createRandomChunk( -3, 1 );
+    m_terrain[ 18 ] = createRandomChunk( -3, 1 );
     m_terrain[ 0 ].m_neighbours[ 0 ] = 1;
     m_terrain[ 0 ].m_neighbours[ 1 ] = 2;
     m_terrain[ 0 ].m_neighbours[ 2 ] = 3;
     m_terrain[ 0 ].m_neighbours[ 3 ] = 4;
     m_terrain[ 0 ].m_neighbours[ 4 ] = 5;
     m_terrain[ 0 ].m_neighbours[ 5 ] = 6;
+    m_terrain[ 1 ].m_neighbours[ 0 ] = 7;
+    m_terrain[ 1 ].m_neighbours[ 1 ] = 13;
+    m_terrain[ 2 ].m_neighbours[ 1 ] = 8;
+    m_terrain[ 2 ].m_neighbours[ 2 ] = 14;
+    m_terrain[ 3 ].m_neighbours[ 2 ] = 9;
+    m_terrain[ 3 ].m_neighbours[ 3 ] = 15;
+    m_terrain[ 4 ].m_neighbours[ 3 ] = 10;
+    m_terrain[ 4 ].m_neighbours[ 4 ] = 16;
+    m_terrain[ 5 ].m_neighbours[ 4 ] = 11;
+    m_terrain[ 5 ].m_neighbours[ 5 ] = 17;
+    m_terrain[ 6 ].m_neighbours[ 5 ] = 12;
+    m_terrain[ 6 ].m_neighbours[ 0 ] = 18;
 }
 
 //------------------------------------------------------------------------------
