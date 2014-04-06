22d3aa76432fd88c7b565128f38a5af70a9a750f
Luis Cabellos <zhen.sydow@gmail.com>
2014-04-06 16:31:16 +0200

    generate tile division tool

--- tools/main_genhworld.cpp
+++ tools/main_genhworld.cpp
@@ -23,10 +23,130 @@
 */
 //------------------------------------------------------------------------------
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include "glminc.hpp"
+#include "constants.hpp"
+#include "chunk.hpp"
+
+//------------------------------------------------------------------------------
+using namespace std;
+
+//------------------------------------------------------------------------------
+bool equalCenter( const glm::vec2 & a, const glm::vec2 & b ){
+    if( abs( a.x - b.x ) >= 0.01f ){
+        return false;
+    }
+
+    if( abs( a.y - b.y ) >= 0.01f ){
+        return false;
+    }
+
+    return true;
+}
+
+//------------------------------------------------------------------------------
+ChunkID mirrorNeigh( ChunkID orig ){
+    return (orig + 3) % Chunk::NNEIGHBOURS;
+}
+
+//------------------------------------------------------------------------------
+vector<Chunk> genTileDivision( const glm::vec2 & a, const glm::vec2 & b ){
+    using ParentChunk = tuple< ChunkID, unsigned int >;
+
+    vector<Chunk> tiles;
+
+    auto w = b.x - a.x;
+    auto h = b.y - a.y;
+    float estimated = (w/5.0) * (h/5.0);
+    float lastPercent = 0.0f;
+
+    cout << estimated << endl;
+
+    auto qwork = queue< pair< ParentChunk, glm::vec2 > >();
+
+    qwork.emplace( make_pair( make_tuple( CHUNK_NULL_IDX, 0 ), a ) );
+
+    ParentChunk parent;
+    glm::vec2 pos;
+    while( not qwork.empty() ){
+        tie( parent, pos ) = qwork.front();
+        auto pid = get<0>(parent);
+        auto plink = get<1>(parent);
+        qwork.pop();
+
+        if( pos.x >= a.x and pos.y >= a.y and pos.x <= b.x and pos.y <= b.y ){
+            unsigned int k = 0;
+            for( k = 0 ; k < tiles.size() ; ++k ){
+                if( equalCenter( tiles[k].m_pos, pos ) ){
+                    break;
+                }
+            }
+
+            if( k != tiles.size() ){
+                auto & cchunk = tiles[ k ];
+                auto clink = mirrorNeigh( plink );
+                cchunk.m_neighbours[ clink ] = pid;
+                auto & pchunk = tiles[ pid ];
+                pchunk.m_neighbours[ plink ] = k;
+            }else{
+                Chunk chunk;
+                chunk.m_pos = pos;
+                auto ownlink = mirrorNeigh( plink );
+                chunk.m_neighbours[ ownlink ] = pid;
+
+                tiles.push_back( chunk );
+
+                auto ownid = tiles.size() - 1;
+                auto & pchunk = tiles[ pid ];
+                pchunk.m_neighbours[ plink ] = ownid;
+
+                for( unsigned int i = 0 ; i < Chunk::NNEIGHBOURS ; ++i ){
+                    if( chunk.m_neighbours[ i ] == CHUNK_NULL_IDX ){
+                        auto offset = glm::rotate( glm::vec2( 1.5, -(5./2.)*sqrt3 ),
+                                                   sixthPart * i );
+                        auto newPos = chunk.m_pos + offset;
+                        qwork.emplace( make_pair( make_tuple( ownid, i ),
+                                                  newPos ) );
+                    }
+                }
+
+                auto currentPercent = 100.f * (float(ownid) / estimated);
+                if( currentPercent > (lastPercent + 5.0f ) ){
+                    cout << int(currentPercent) << " " << flush;
+                    lastPercent = currentPercent;
+                }
+            }
+        }
+    }
+
+    cout << endl;
+
+    return tiles;
+}
 
 //------------------------------------------------------------------------------
 int main(){
-    std::cout << "Generating HWorld" << std::endl;
+    std::cout << "Generating Tile Map" << std::endl;
+
+    auto tiles = genTileDivision( glm::vec2( 0, 0 ), glm::vec2( 500, 500 ) );
+
+    std::cout << std::endl << "Total Tiles : " << tiles.size() << std::endl;
+
+    for( auto & t: tiles ){
+        std::cout << "Tile " << t.m_pos.x << ", " << t.m_pos.y << " ";
+
+        std::cout << "nns = ";
+        for( auto & nn: t.m_neighbours ){
+            if( nn == CHUNK_NULL_IDX ){
+                std::cout << "- ";
+            }else{
+                std::cout << nn << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
